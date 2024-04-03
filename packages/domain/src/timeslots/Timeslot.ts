bdeed85426746932e4c798327953bda7cc21fb5a
ygyg70 <yonatan.goraly@gmail.com>
2024-04-02 18:02:14 -0700

    style: move prettier config to .eslintrc as it was ignored by Intellij Change prettier config Added .editorconfig for IDE style integration

--- packages/domain/src/timeslots/Timeslot.ts
+++ packages/domain/src/timeslots/Timeslot.ts
@@ -1,45 +1,56 @@
-import { Description, Example, Format, Groups, JsonFormatTypes, MaxLength, MinLength, Name, Required, RequiredGroups } from "@tsed/schema";
+import {
+  Description,
+  Example,
+  Format,
+  Groups,
+  JsonFormatTypes,
+  MaxLength,
+  MinLength,
+  Name,
+  Required,
+  RequiredGroups
+} from "@tsed/schema"
 
-import { AllowEmpty } from "../decorators/AllowEmpty.js";
+import { AllowEmpty } from "../decorators/AllowEmpty.js"
 
 export class Timeslot {
   @Format(JsonFormatTypes.UUID)
   @Required()
   @RequiredGroups("update")
-  id: string;
+  id: string
 
   @Required()
   @Name("start_date")
   @Format("date-time")
-  startDate: Date;
+  startDate: Date
 
   @Required()
   @Name("end_date")
   @Format("date-time")
-  endDate: Date;
+  endDate: Date
 
   @Required()
   @Example("Hiking")
   @MinLength(3)
   @MaxLength(100)
-  label: string;
+  label: string
 
   @Required()
   @AllowEmpty()
   @Description("Activity or Dresscode in HTML format")
   @MinLength(3)
   @MaxLength(500)
-  description: string = "";
+  description: string = ""
 
   @Required()
   @Name("created_at")
   @Format("date-time")
   @Groups("!update", "!create")
-  createdAt: Date = new Date();
+  createdAt: Date = new Date()
 
   @Required()
   @Name("updated_at")
   @Format("date-time")
   @Groups("!update", "!create")
-  updatedAt: Date = new Date();
+  updatedAt: Date = new Date()
 }
