bfb151df3bcaed2d79921642fca05e619c904eb2
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-05-03 10:48:58 +0200

    feat(infra): update TimeslotsRepository to persist data in file

--- packages/infra/src/timeslots/TimeslotsRepository.spec.ts
+++ packages/infra/src/timeslots/TimeslotsRepository.spec.ts
@@ -1,8 +1,13 @@
+import { dirname } from "node:path"
+
 import { Timeslot } from "@project/domain/timeslots/Timeslot.js"
 import { DITest } from "@tsed/di"
+import fs from "fs-extra"
 import { beforeEach } from "vitest"
 
 import { TimeslotsRepository } from "./TimeslotsRepository.js"
+
+vi.mock("fs-extra")
 
 async function getServiceFixture() {
   const service = await DITest.invoke<TimeslotsRepository>(TimeslotsRepository, [])
@@ -11,40 +16,105 @@ async function getServiceFixture() {
 }
 
 describe("TimeslotsRepository", () => {
-  beforeEach(async () => {
-    await DITest.create()
-    vi.useFakeTimers({ now: new Date("2021-01-01T00:10:00.000Z") })
-  })
-  afterEach(async () => {
-    await DITest.reset()
-    vi.useRealTimers()
-  })
+  describe("$onInit", () => {
+    beforeEach(async () => {
+      vi.mocked(fs.readJson as unknown as () => Promise<unknown[]>).mockResolvedValue([
+        {
+          id: "id",
+          label: "Hiking",
+          description: "Hiking in the mountains",
+          start_date: "2021-01-01T10:00:00.000Z",
+          end_date: "2021-01-01T11:00:00.000Z",
+          created_at: "2021-01-01T00:00:00.000Z",
+          updated_at: "2021-01-01T00:00:00.000Z"
+        }
+      ])
 
-  describe("getById()", () => {
-    it("should return the timeslot with the given id", async () => {
-      const { service } = await getServiceFixture()
+      await DITest.create({
+        timeslots: {
+          dbFilePath: "./dir/timeslots.json"
+        }
+      })
 
-      const timeslot = new Timeslot()
-      timeslot.label = "Hiking"
-      timeslot.description = "Hiking in the mountains"
-      timeslot.startDate = new Date("2021-01-01T10:00:00.000Z")
-      timeslot.endDate = new Date("2021-01-01T11:00:00.000Z")
-
-      const newTimeslot = await service.create(timeslot)
-
-      const result = await service.getById(newTimeslot.id)
-
-      expect(result).toEqual(newTimeslot)
-      expect(result?.createdAt).toEqual(new Date("2021-01-01T00:10:00.000Z"))
-      expect(result?.updatedAt).toEqual(new Date("2021-01-01T00:10:00.000Z"))
+      vi.useFakeTimers({ now: new Date("2021-01-01T00:10:00.000Z") })
+    })
+    afterEach(async () => {
+      await DITest.reset()
+      vi.useRealTimers()
     })
 
-    it("should not return a timeslot when the given id is unknown", async () => {
-      const { service } = await getServiceFixture()
+    it("should check if the file exists and create it", async () => {
+      const repository = await DITest.get(TimeslotsRepository, [])
 
-      const result = await service.getById("id")
+      expect(fs.existsSync).toHaveBeenCalledWith("./dir/timeslots.json")
+      expect(fs.ensureDir).toHaveBeenCalledWith(dirname("./dir/timeslots.json"))
+      expect(fs.writeJson).toHaveBeenCalledWith("./dir/timeslots.json", [])
+      expect(fs.readJson).toHaveBeenCalledWith("./dir/timeslots.json", {
+        encoding: "utf-8"
+      })
 
-      expect(result).toEqual(undefined)
+      expect(repository.cache.size).toBe(1)
+
+      const timeslot = repository.cache.get("id")
+
+      repository.save(timeslot)
+
+      expect(fs.writeJson).toHaveBeenCalledWith(
+        "./dir/timeslots.json",
+        [
+          {
+            created_at: "2021-01-01T00:00:00.000Z",
+            description: "Hiking in the mountains",
+            end_date: "2021-01-01T11:00:00.000Z",
+            id: "id",
+            label: "Hiking",
+            start_date: "2021-01-01T10:00:00.000Z",
+            updated_at: "2021-01-01T00:10:00.000Z"
+          }
+        ],
+        {
+          EOL: "\n",
+          spaces: 2
+        }
+      )
+    })
+  })
+  describe("methods", () => {
+    beforeEach(async () => {
+      await DITest.create()
+      vi.useFakeTimers({ now: new Date("2021-01-01T00:10:00.000Z") })
+    })
+    afterEach(async () => {
+      await DITest.reset()
+      vi.useRealTimers()
+    })
+
+    describe("getById()", () => {
+      it("should return the timeslot with the given id", async () => {
+        const { service } = await getServiceFixture()
+
+        const timeslot = new Timeslot()
+        timeslot.label = "Hiking"
+        timeslot.description = "Hiking in the mountains"
+        timeslot.startDate = new Date("2021-01-01T10:00:00.000Z")
+        timeslot.endDate = new Date("2021-01-01T11:00:00.000Z")
+
+        const newTimeslot = await service.create(timeslot)
+
+        const result = await service.getById(newTimeslot.id)
+
+        expect(result).toEqual(newTimeslot)
+        expect(result?.createdAt).toEqual(new Date("2021-01-01T00:10:00.000Z"))
+        expect(result?.updatedAt).toEqual(new Date("2021-01-01T00:10:00.000Z"))
+      })
+
+      it("should not return a timeslot when the given id is unknown", async () => {
+        const { service } = await getServiceFixture()
+
+        const result = await service.getById("id")
+
+        expect(result).toEqual(undefined)
+      })
     })
   })
 })
--- packages/infra/src/timeslots/TimeslotsRepository.ts
+++ packages/infra/src/timeslots/TimeslotsRepository.ts
@@ -1,34 +1,89 @@
+import { dirname } from "node:path"
+
 import { Timeslot } from "@project/domain/timeslots/Timeslot.js" // use absolute import to optimize the bundle tree shaking
-import { Injectable } from "@tsed/di"
+import { Constant, Inject, Injectable } from "@tsed/di"
+import { deserialize, serialize } from "@tsed/json-mapper"
+import { Logger } from "@tsed/logger"
+import fs from "fs-extra"
 import { v4 } from "uuid"
 
 @Injectable()
 export class TimeslotsRepository {
+  @Constant("timeslots.dbFilePath", "")
+  private timeslotsFilePath: string
+
   private cache = new Map<string, Timeslot>()
+
+  @Inject()
+  private logger: Logger
+
+  async $onInit() {
+    if (this.timeslotsFilePath) {
+      if (!fs.existsSync(this.timeslotsFilePath)) {
+        this.logger.info({
+          event: "TIMESLOTS_REPOSITORY",
+          message: "Timeslots database file not found. Creating a new one...",
+          filePath: this.timeslotsFilePath
+        })
+        await fs.ensureDir(dirname(this.timeslotsFilePath))
+        await fs.writeJson(this.timeslotsFilePath, [])
+      }
+
+      const raw = await fs.readJson(this.timeslotsFilePath, {
+        encoding: "utf-8"
+      })
+
+      for (const timeslot of raw) {
+        this.cache.set(timeslot.id, deserialize(timeslot, { type: Timeslot }))
+      }
+    }
+  }
 
   getById(id: string) {
     return Promise.resolve(this.cache.get(id))
   }
 
-  getAll() {
-    return Promise.resolve(Array.from(this.cache.values()))
+  getAll(queryOpts?: Partial<{ limit: number }>) {
+    const timeslots = [...this.cache.values()]
+
+    if (queryOpts?.limit) {
+      return Promise.resolve(timeslots.slice(0, queryOpts.limit))
+    }
+
+    return Promise.resolve(timeslots)
   }
 
-  create(timeslot: Timeslot) {
+  async create(timeslot: Timeslot) {
     timeslot.id = v4()
     timeslot.createdAt = new Date()
     timeslot.updatedAt = new Date()
 
     this.cache.set(timeslot.id, timeslot)
+    await this.saveToDisk()
 
-    return Promise.resolve(timeslot)
+    return timeslot
   }
 
-  save(timeslot: Timeslot) {
+  async save(timeslot: Timeslot) {
     timeslot.updatedAt = new Date()
 
     this.cache.set(timeslot.id, timeslot)
+    await this.saveToDisk()
 
-    return Promise.resolve(timeslot)
+    return timeslot
+  }
+
+  clear() {
+    this.cache.clear()
+    return this.saveToDisk()
+  }
+
+  protected async saveToDisk() {
+    if (this.timeslotsFilePath) {
+      await fs.writeJson(this.timeslotsFilePath, serialize([...this.cache.values()]), {
+        spaces: 2,
+        EOL: "\n"
+      })
+    }
   }
 }
