0c1d333fcec6a7473c09d69627333d4f9524d4a5
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-06-20 09:16:06 +0200

    feat(controller): add LambdaAuthorizerController to handle aws custom auth invocation

---
+++ packages/controllers/auth/LambdaAuthorizerController.spec.ts
@@ -0,0 +1,100 @@
+import { UserInfo } from "@project/domain/users/UserInfo.js"
+import { JwtService } from "@project/infra/auth/services/JwtService.js"
+import { PlatformTest } from "@tsed/common"
+import { DITest } from "@tsed/di"
+import { ServerlessContext, ServerlessResponse } from "@tsed/platform-serverless"
+
+import { AuthorizeServerlessContext, LambdaAuthorizerController } from "./LambdaAuthorizerController.js"
+
+async function getControllerFixture({ scopes }: { scopes: string[] }) {
+  const controller = await PlatformTest.invoke<LambdaAuthorizerController>(LambdaAuthorizerController, [])
+  const user = new UserInfo({
+    _id: "123",
+    email: "",
+    scopes
+  })
+
+  const jwtService = PlatformTest.get<JwtService>(JwtService)
+  const authorizationToken = await jwtService.encode({ user })
+
+  const $ctx = new ServerlessContext({
+    id: "id",
+    injector: DITest.injector,
+    logger: DITest.injector.logger,
+    event: {
+      authorizationToken
+    } as never,
+    context: {} as never,
+    endpoint: {} as never
+  })
+
+  return {
+    user,
+    controller,
+    $ctx
+  }
+}
+
+describe("LambdaAuthorizerController", () => {
+  beforeEach(() =>
+    DITest.create({
+      envs: {
+        JWT_SECRET: "zH4NRP1HMALxxCFnRZABFA7GOJtzU_gIj02alfL1lvI",
+        JWT_ISSUER: "https://egain.com",
+        JWT_AUDIENCE: "537d714c-d062-45ef-957d-6beac6490233"
+      }
+    })
+  )
+  afterEach(() => DITest.reset())
+
+  describe("authorizer()", () => {
+    it("should allow user to consume api", async () => {
+      const { controller, $ctx } = await getControllerFixture({
+        scopes: ["api", "timeslots"]
+      })
+
+      const result = await controller.authorizer($ctx as AuthorizeServerlessContext)
+
+      expect(result).toEqual({
+        context: {
+          user: {
+            email: "",
+            emailVerified: false,
+            id: "123",
+            scopes: ["api", "timeslots"]
+          }
+        },
+        principalId: "me"
+      })
+    })
+    it("should reject user to consume timeslots", async () => {
+      const { controller, $ctx } = await getControllerFixture({
+        scopes: ["api"]
+      })
+
+      const result = await controller.authorizer($ctx as AuthorizeServerlessContext)
+
+      expect(result).toEqual({
+        context: {
+          user: undefined
+        },
+        principalId: "me"
+      })
+    })
+    it("should reject user to consume api", async () => {
+      const { controller, $ctx } = await getControllerFixture({
+        scopes: []
+      })
+
+      const result = (await controller.authorizer($ctx as AuthorizeServerlessContext)) as unknown as ServerlessResponse
+
+      expect(result.statusCode).toEqual(401)
+      expect(result.getBody()).toEqual({
+        context: {
+          user: undefined
+        },
+        principalId: "me"
+      })
+    })
+  })
+})
---
+++ packages/controllers/auth/LambdaAuthorizerController.ts
@@ -0,0 +1,42 @@
+import { JwtService } from "@project/infra/auth/services/JwtService.js"
+import { AwsPolicyService } from "@project/infra/aws/auth/AwsPolicyService.js"
+import { Controller, Inject } from "@tsed/di"
+import { Context } from "@tsed/platform-params"
+import { ServerlessContext } from "@tsed/platform-serverless"
+import { Description, Get } from "@tsed/schema"
+
+export type AuthorizeServerlessContext = ServerlessContext & {
+  event: { authorizationToken: string; methodArn: string }
+}
+
+@Controller("/")
+export class LambdaAuthorizerController {
+  @Inject()
+  protected awsPolicyService: AwsPolicyService
+
+  @Inject()
+  protected jwtService: JwtService
+
+  @Get("/authorizer")
+  @Description("this endpoint is used to authorize the request using Lambda aws")
+  async authorizer(@Context() $ctx: AuthorizeServerlessContext) {
+    const decodedToken = await this.jwtService.decode($ctx.event.authorizationToken)
+    const { user } = decodedToken.payload
+
+    // example on how to manage auth and format the response depending on the user scope
+    if (!user.scopes.includes("api")) {
+      // not allowed to consume API
+      return $ctx.response.status(401).body(this.awsPolicyService.generateDeny("me", $ctx.event.methodArn))
+    }
+
+    if (user.scopes.includes("timeslots")) {
+      // allowed to consume timeslots
+      return this.awsPolicyService.generateAllow("me", $ctx.event.methodArn, {
+        user
+      })
+    }
+
+    // no API resource allowed
+    return this.awsPolicyService.generateDeny("me", $ctx.event.methodArn)
+  }
+}
