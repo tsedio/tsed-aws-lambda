69d0816de3311468ede2559c72996687d171b17f
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-06-22 11:52:58 +0200

    feat(controller): use Authorize decorator to protected endpoint

--- packages/controllers/timeslots/TimeslotsController.spec.ts
+++ packages/controllers/timeslots/TimeslotsController.spec.ts
@@ -1,5 +1,8 @@
 import { Timeslot } from "@project/domain/timeslots/Timeslot.js"
+import { UserInfo } from "@project/domain/users/UserInfo.js"
+import { JwtService } from "@project/infra/auth/services/JwtService.js"
 import { TimeslotsRepository } from "@project/infra/timeslots/TimeslotsRepository.js"
+import { PlatformTest, runInContext } from "@tsed/common"
 import { catchAsyncError } from "@tsed/core"
 import { DITest } from "@tsed/di" // we use DITest to be agnostic with the Serverless/Web platform as much is possible
 import { NotFound } from "@tsed/exceptions"
@@ -30,15 +33,35 @@ async function getControllerFixture() {
     }
   ])
 
+  const jwtService = DITest.get<JwtService>(JwtService)
+  const token = await jwtService.encode({
+    user: new UserInfo({
+      scopes: ["timeslots"]
+    })
+  })
+
+  const $ctx = PlatformTest.createRequestContext()
+
+  $ctx.request.headers["authorization"] = "Bearer " + token
+
   return {
     controller,
     repository,
-    timeslot
+    timeslot,
+    $ctx
   }
 }
 
 describe("TimeslotsController", () => {
-  beforeEach(() => DITest.create())
+  beforeEach(() =>
+    DITest.create({
+      envs: {
+        JWT_SECRET: "zH4NRP1HMALxxCFnRZABFA7GOJtzU_gIj02alfL1lvI",
+        JWT_ISSUER: "https://egain.com",
+        JWT_AUDIENCE: "537d714c-d062-45ef-957d-6beac6490233"
+      }
+    })
+  )
   afterEach(() => DITest.reset())
 
   describe("OAS3", () => {
@@ -53,9 +76,9 @@ describe("TimeslotsController", () => {
 
   describe("getTimeslots()", () => {
     it("should return the list of timeslots", async () => {
-      const { timeslot, controller, repository } = await getControllerFixture()
+      const { timeslot, $ctx, controller, repository } = await getControllerFixture()
 
-      const result = await controller.getTimeslots()
+      const result = await runInContext($ctx, () => controller.getTimeslots())
 
       expect(repository.getAll).toHaveBeenCalledWith()
       expect(result).toEqual([timeslot])
@@ -64,20 +87,20 @@ describe("TimeslotsController", () => {
 
   describe("getTimeslotById()", () => {
     it("should return the timeslot from his given id", async () => {
-      const { timeslot, controller, repository } = await getControllerFixture()
+      const { timeslot, controller, $ctx, repository } = await getControllerFixture()
 
-      const result = await controller.getTimeslotById("uuid")
+      const result = await runInContext($ctx, () => controller.getTimeslotById("uuid"))
 
       expect(repository.getById).toHaveBeenCalledWith("uuid")
       expect(result).toEqual(timeslot)
     })
 
     it("should throw a not found error when the given id isn't known", async () => {
-      const { controller, repository } = await getControllerFixture()
+      const { controller, repository, $ctx } = await getControllerFixture()
 
       repository.getById.mockResolvedValue(undefined)
 
-      const error = await catchAsyncError<NotFound>(() => controller.getTimeslotById("uuid"))
+      const error = await catchAsyncError<NotFound>(() => runInContext($ctx, () => controller.getTimeslotById("uuid")))
 
       expect(repository.getById).toHaveBeenCalledWith("uuid")
       expect(error).toBeInstanceOf(NotFound)
@@ -87,9 +110,9 @@ describe("TimeslotsController", () => {
 
   describe("createTimeslot()", () => {
     it("should create a new timeslot", async () => {
-      const { timeslot, controller, repository } = await getControllerFixture()
+      const { timeslot, controller, repository, $ctx } = await getControllerFixture()
 
-      const result = await controller.createTimeslot(timeslot)
+      const result = await runInContext($ctx, () => controller.createTimeslot(timeslot))
 
       expect(repository.create).toHaveBeenCalledWith(timeslot)
       expect(result).toEqual(timeslot)
@@ -98,20 +121,20 @@ describe("TimeslotsController", () => {
 
   describe("updateTimeslot()", () => {
     it("should update a timeslot", async () => {
-      const { timeslot, controller, repository } = await getControllerFixture()
+      const { timeslot, controller, $ctx, repository } = await getControllerFixture()
 
-      const result = await controller.updateTimeslot(timeslot.id, timeslot)
+      const result = await runInContext($ctx, () => controller.updateTimeslot(timeslot.id, timeslot))
 
       expect(repository.save).toHaveBeenCalledWith(timeslot)
       expect(result).toEqual(timeslot)
     })
 
     it("should throw a not found error when the given id isn't known", async () => {
-      const { timeslot, controller, repository } = await getControllerFixture()
+      const { timeslot, controller, repository, $ctx } = await getControllerFixture()
 
       repository.getById.mockResolvedValue(undefined)
 
-      const error = await catchAsyncError<NotFound>(() => controller.updateTimeslot("uuid", timeslot))
+      const error = await catchAsyncError<NotFound>(() => runInContext($ctx, () => controller.updateTimeslot("uuid", timeslot)))
 
       expect(repository.getById).toHaveBeenCalledWith(timeslot.id)
       expect(error).toBeInstanceOf(NotFound)
